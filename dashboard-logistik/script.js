68d773f11d8b674680d355fad6563e97f5beaf5f
hendrayudha0903 <hendrayudha0903@gmail.com>
2026-06-15 11:52:27 +0700

    Simpan file lokal TaniMakmur

---
+++ dashboard-logistik/script.js
@@ -0,0 +1,64 @@
+const lineCtx = document.getElementById('lineChart');
+
+new Chart(lineCtx, {
+    type: 'line',
+    data: {
+        labels: lineLabel,
+        datasets: [{
+            label: 'Total Panen (Kg)',
+            data: lineData,
+            borderColor: '#2f8f3d',
+            backgroundColor: 'rgba(47,143,61,0.2)',
+            tension: 0.4,
+            fill: true
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+    }
+});
+
+const doughnutCtx = document.getElementById('doughnutChart');
+
+new Chart(doughnutCtx, {
+    type: 'doughnut',
+    data: {
+        labels: statusLabel,
+        datasets: [{
+            data: statusData,
+            backgroundColor: [
+    '#fbbc05',
+    '#4285f4',
+    '#34a853',
+    '#ea4335'
+]
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+    }
+});
+
+const barCtx = document.getElementById('barChart');
+
+new Chart(barCtx, {
+    type: 'bar',
+    data: {
+        labels: gradeLabel,
+        datasets: [{
+            label: 'Volume Panen (Kg)',
+            data: gradeData,
+            backgroundColor: [
+                '#34a853',
+                '#fbbc05',
+                '#ea4335'
+            ]
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+    }
+});
